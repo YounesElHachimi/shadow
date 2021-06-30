554f382273191fc5e5af8b70c38f998dee960687
younes el hachimi <younes.al.hachimi@gmail.com>
2021-06-30 14:59:59 +0000

    inittialize project

---
+++ script.js
@@ -0,0 +1,15 @@
+function shadow(options) {
+  console.log("execute");
+
+  let boxs = document.querySelectorAll(".box");
+  boxs.forEach((box) => {
+    console.log(box);
+    let size = "10px";
+    if (options.size) {
+      size = options.size + "px";
+    }
+    box.style.boxShadow = "0 0 " + size + " 1px #ccc";
+  });
+}
+
+module.export.shadow = shadow;
